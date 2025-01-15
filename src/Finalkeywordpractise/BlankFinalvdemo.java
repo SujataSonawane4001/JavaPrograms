0f9d0206da9025153f735fff29e789637ad492b4
SujataSonawane4001 <sujata62sonawane@gmail.com>
2025-01-15 15:40:40 +0530

    All mentorship programs

---
+++ src/Finalkeywordpractise/BlankFinalvdemo.java
@@ -0,0 +1,44 @@
+package Finalkeywordpractise;
+
+public class BlankFinalvdemo {
+	
+	final int i; //instance
+	  
+//	  static{
+//		  i=20;
+//		  System.out.println(i);
+//	  }
+	  
+//	  public BlankFinalvdemo(int i) {
+//		  this.i=i;
+//	  }
+//	  public BlankFinalvdemo() {
+//		i=10;
+//		System.out.println(i);
+//	  }
+	  
+	
+//	static{
+//		i=10;
+//	}
+	
+	public BlankFinalvdemo() {
+		i=10;
+	}
+//	static{
+//		i=10;
+//		System.out.println(i);	
+//	}
+	
+
+	public static void main(String[] args) {
+		BlankFinalvdemo b=new BlankFinalvdemo();
+	//	b.i=20;
+
+	}
+
+}
+
+//Staic block
+//constructor
+// instance block
---
+++ src/Polymorphism/Demo1.java
@@ -0,0 +1,29 @@
+package Polymorphism;
+
+public class Demo1 {
+	
+	private void m1(float x,int y) {    //overloading
+		// TODO Auto-generated method stub
+
+	}
+	private void m1(int y,float x) {    //overloading
+		// TODO Auto-generated method stub
+
+	}
+	private void m1() {
+		
+
+	}
+   public static void main(String[] args) {
+	
+  }
+}
+
+
+//polymorphism-->
+//One into many form
+//single name multiple form
+//Polymorphism means creating multiple forms of same things
+
+//Rules:
+//same method name but different parameters/List of arguments-->data types must be different
